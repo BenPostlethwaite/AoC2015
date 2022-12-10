fc896fd6a5e166e23786f7b1ae41f3a2658e77c0
Ben Postlethwaite <Ben.Postlethwaite05@gmail.com>
2022-12-10 14:34:42 +0000

    first commit

---
+++ AoC day 2 2015/AoC day 2 2015/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AoC_day_2_2015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+            List<int> b = new List<int>();
+            List<int> c = new List<int>();
+
+            string[] dataArray = File.ReadAllLines("input.txt");
+            foreach (string line in dataArray)
+            {
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = File.ReadAllLines("test.txt");
+            int[] pos = { 0, 0 };
+            List<int[]> houses = new List<int[]>();
+            foreach (char c in data[0])
+            {
+                if (c == '>')
+                {
+                    pos[0]++;
+                }
+                else if (c == '<')
+                {
+                    pos[0]--;
+                }
+                else if (c == '^')
+                {
+                    pos[1]++;
+                }
+                else if (c == 'v')
+                {
+                    pos[1]--;
+                }
+
+                if (houses.Contains(c) == false)
+                {
+                    houses.Add(pos);
+                }
+            }
+        }
+    }
+}
